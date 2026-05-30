75f76b67b58401c190ae87151b68e91afb0a50af
mi2428 <mi2428782020@gmail.com>
2026-05-30 18:59:11 +0900

    fing 0.11.0

--- Formula/fing.rb
+++ Formula/fing.rb
@@ -4,21 +4,21 @@
 class Fing < Formula
   desc "Local IPv4 network scanner with device fingerprints"
   homepage "https://github.com/mi2428/fing"
-  version "0.10.0"
+  version "0.11.0"
   license "MIT"
   depends_on :macos
 
   on_macos do
     on_arm do
-      url "https://github.com/mi2428/fing/releases/download/v0.10.0/fing-v0.10.0-darwin-arm64",
+      url "https://github.com/mi2428/fing/releases/download/v0.11.0/fing-v0.11.0-darwin-arm64",
           using: :nounzip
-      sha256 "bc44974d1661248d78f6dce38bfd9546f6c030da19c10e95739e0779bf9fe5ef"
+      sha256 "12ef70b787c76b795e170241bfa6de271935978e3a2a5f2e56bbaee1c4b202c8"
     end
 
     on_intel do
-      url "https://github.com/mi2428/fing/releases/download/v0.10.0/fing-v0.10.0-darwin-amd64",
+      url "https://github.com/mi2428/fing/releases/download/v0.11.0/fing-v0.11.0-darwin-amd64",
           using: :nounzip
-      sha256 "7035ccc82a8b0c97557d0320c96b044f3dfec65b3b5f73ddd24f97ac9afc2e34"
+      sha256 "4f9098c35faeb3dcd00f58fbe775d45459a46b190290197541625701ce8129a8"
     end
   end
 
